943354adfcd849315ed6c9568762fec9b0362ab3
Javijaramillo9221 <javierjaramillo9221@gmail.com>
2026-04-23 22:24:44 -0500

    configuracion base de datos supabase

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    # Ensure compatible connection string for SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres.psmxbbbyczfintaelryf:Lmhrs%2A2026_db@aws-1-us-east-2.pooler.supabase.com:5432/postgres"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'dev-key-petgroom'
-            
