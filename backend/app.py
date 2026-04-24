6f6d0cb803dcb9ebe5e1d974c40586c86341d266
Javijaramillo9221 <javierjaramillo9221@gmail.com>
2026-04-24 18:19:02 -0500

    fix import config

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from backend.config import Config
+from config import Config
 from backend.models import db
 from backend.routes import register_blueprints
 
